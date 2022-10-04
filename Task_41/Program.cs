73fa5e52b37688c4fbc5f53bb5f90b15c5407c00
Sasha <sasha@example.com>
2022-10-04 18:49:23 +0300

    количество цифр больше нуля

---
+++ Task_41/Program.cs
@@ -0,0 +1,24 @@
+﻿/*  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+-1, -7, 567, 89, 223-> 3 */
+Console.WriteLine("Введите количество чисел :");
+int M = Convert.ToInt32(Console.ReadLine());
+int total = 0;
+string numbers = " ";
+while (M > 0)
+{
+    Console.WriteLine("Введите число ");
+    int usernumber = Convert.ToInt32(Console.ReadLine());
+    if(usernumber > 0)
+    {
+        total++;
+    }
+    numbers += usernumber + " ";
+    
+    M = M - 1;
+
+}
+Console.WriteLine(numbers + "--> " + total);
+
