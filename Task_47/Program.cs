eb4e1f55d8e7ee8eddc763437d668b3627d370d4
Sasha <sasha@example.com>
2022-10-08 14:41:48 +0300

    двумерный массив вещественных чисел

---
+++ Task_47/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Double[,] Array2D(int m,int n)
+{
+    Double[,] twoDArray = new double [m,n];
+    
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            twoDArray[i,j] = new Random().NextDouble() * 10;
+            twoDArray[i,j] = Math.Round(twoDArray[i,j],2);
+        }
+    }
+    return twoDArray;
+}
+void prDouble2DArray(Double[,] arrayToPrDouble)
+{
+    
+    for (int i = 0; i < arrayToPrDouble.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayToPrDouble.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrDouble[i,j]  + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Double[,] generatedArray = Array2D(5,5);
+prDouble2DArray(generatedArray);
